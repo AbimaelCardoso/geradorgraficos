a92be0ced604566802440ad02eeea1a7c997ed6b
AbimaelCardoso <42761473+AbimaelCardoso@users.noreply.github.com>
2025-04-02 00:21:58 +0000

    Preparação para implantação

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import base64
 from datetime import date
 import matplotlib
 matplotlib.use('Agg')
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
 
 app = FastAPI(title="COFIPEI API", description="API para geração de gráficos financeiros")
 
